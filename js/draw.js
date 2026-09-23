e25d7c73074b5a8ed36178b7dcabc5fbf13ec0e7
tyoshida18 <tsyoshida_msasd@magnoliascience.org>
2026-09-23 18:18:52 +0000

    Update ball color and background

--- js/draw.js
+++ js/draw.js
@@ -117,7 +117,7 @@ Draw.player = function () {
   var centerY = Player.y + CONFIG.PLAYER_SIZE / 2;
 
   // the circle
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = "#ffd700";
   ctx.strokeStyle = "#000000";
   ctx.lineWidth = CONFIG.LINE_WIDTH;
   ctx.beginPath();
