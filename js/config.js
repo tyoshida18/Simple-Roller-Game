a184fd614f948bd65cf6938bea5c752484e2a368
tyoshida18 <tsyoshida_msasd@magnoliascience.org>
2026-09-23 18:29:54 +0000

    Add more spikes and soften jump boost

--- js/config.js
+++ js/config.js
@@ -20,7 +20,7 @@ var CONFIG = {
   // --- how the player moves -------------------------------------------
   MOVE_SPEED: 5,      // pixels per frame left and right
   JUMP_POWER: 18,     // how hard the jump pushes UP. bigger = higher
-  JUMP_SIDE_BOOST: 2.6, // extra sideways speed while taking off
+  JUMP_SIDE_BOOST: 2.4, // extra sideways speed while taking off
   GRAVITY: 0.8,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall
 
